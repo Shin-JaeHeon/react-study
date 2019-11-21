aada28257e9b774281a9ccc699960c2e349689fa
JaeHeonShin <sjh0624@midasit.com>
2019-11-21 09:27:51 +0900

    update lib/VM

--- src/app/libs/VM.ts
+++ src/app/libs/VM.ts
@@ -5,12 +5,11 @@ export default class VM<T> {
   @observable serviceSeparator: boolean;
 
   constructor(private readonly _service: T, private readonly _service2: T = null) {
-    this.serviceSeparator = true;
   }
 
 
   @computed get service(): T {
-    return this.serviceSeparator ? this._service : (this._service2 ? this._service2 : this._service);
+    return !this.serviceSeparator ? this._service : (this._service2 ? this._service2 : this._service);
   };
 
   getAllService(): ReadonlyArray<T> {
