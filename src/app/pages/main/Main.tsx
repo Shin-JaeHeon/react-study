3048d9847a23748612a140793d67f881e6010927
JaeHeonShin <sjh0624@midasit.com>
2019-11-22 14:47:35 +0900

    PopularTagList Component 제거

--- src/app/pages/main/Main.tsx
+++ src/app/pages/main/Main.tsx
@@ -1,11 +1,11 @@
 import * as style from "./main.less";
 import React from "react";
 import PageList from "../../components/pageList/PageList";
-import PopularTagList from "./view/popularTagList/PopularTagList";
 import {observer} from "mobx-react";
 import FeedList from "../../components/feedList/FeedList";
 import MainVM from "./mainVM";
 import TopUI from "../../components/topUI/TopUI";
+import TagListItem from '../../components/tagList/tagListItem';
 
 @observer
 export default class Main extends React.Component {
@@ -15,7 +15,7 @@ export default class Main extends React.Component {
     if (page === 0) this.vm.clickedTag = '';
     this.vm.topPage = page;
   };
-  pageClicked = page =>this.vm.page = page;
+  pageClicked = page => this.vm.page = page;
   likeClicked = (i: number) => this.vm.updateLike(i);
 
   componentDidMount() {
@@ -40,10 +40,15 @@ export default class Main extends React.Component {
             />
             <FeedList articleList={articleList} handler={this.likeClicked}/>
           </div>
-          <PopularTagList tagList={popularTagList} handler={this.tagClicked}/>
+          <div className={style.tags}>
+            <h4>Popular Tags</h4>
+            {popularTagList.map((tag, i) => (
+              <TagListItem tag={tag} key={i} onClick={this.tagClicked(tag)}/>
+            ))}
+          </div>
         </section>
         <footer>
-         <PageList pageList={pageList} handler={this.pageClicked}/>
+          <PageList pageList={pageList} handler={this.pageClicked}/>
         </footer>
       </main>
     );
