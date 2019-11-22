428dadaf6e006aef185b786079cbad381641d72d
JaeHeonShin <sjh0624@midasit.com>
2019-11-22 12:07:38 +0900

    Editor Update

--- src/app/pages/editor/Editor.tsx
+++ src/app/pages/editor/Editor.tsx
@@ -15,7 +15,7 @@ interface Props {
 export default class Editor extends React.Component<Props> {
   readonly vm = EditorVM.instance;
   readonly handler = () => this.vm.update();
-  readonly changed = name => event => this.vm[name] = event.target.value;
+  readonly changed = event => this.vm[event.target.name] = event.target.value;
   readonly entered = event => {
     if (event.key === 'Enter') {
       this.vm.addTag(event.target.value);
@@ -30,17 +30,18 @@ export default class Editor extends React.Component<Props> {
   }
 
   render() {
+    const {changed} = this;
     if (this.vm.redirectID !== '') {
       return <Redirect to={`/article/${this.vm.redirectID}`}/>
     }
     return (
       <div className={style.editorContainer}>
         <div className={style.customForm}>
-          <TextInput placeholder="Title" onChange={this.changed('title')} value={this.vm.title}/>
-          <TextInput placeholder="What's this article about?" onChange={this.changed('description')}
+          <TextInput placeholder="Title" onChange={changed} name="title" value={this.vm.title}/>
+          <TextInput placeholder="What's this article about?" onChange={changed} name="description"
                      value={this.vm.description}/>
           <textarea className={style.customTextArea} placeholder="Write your article (in markdown)"
-                    onChange={this.changed('body')} value={this.vm.body}/>
+                    onChange={changed} name="body" value={this.vm.body}/>
           <TextInput placeholder="Enter tags" onKeyDown={this.entered}/>
           <div className={style.tagsContainer}>
             <TagList tagList={this.vm.tagList} store={this.vm}/>
