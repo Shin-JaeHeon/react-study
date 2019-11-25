8e789bb8294413e337ffafaa763e11e8b98daad4
JaeHeonShin <sjh0624@midasit.com>
2019-11-25 11:33:45 +0900

    PageSplit이 제대로 되지 않았던 오류 수정

--- src/app/models/articleListModel.ts
+++ src/app/models/articleListModel.ts
@@ -1,16 +1,14 @@
 import {observable} from 'mobx';
 import Article, {ArticleVO} from './articleModel';
 
-const ARTICLE_PER_PAGE = 5;
 
 export default class ArticleList {
-	@observable list: Array<Article> = [];
-	@observable pages: ReadonlyArray<number> = [];
+    @observable list: Array<Article> = [];
+    @observable pages: ReadonlyArray<number> = [];
 
-	constructor(articles: ReadonlyArray<ArticleVO>, count: number) {
-		const page = Math.ceil(count / ARTICLE_PER_PAGE);
-
-		this.pages = new Array(page).fill(0).map((_, i) => i);
-		this.list = articles.map(article => new Article(article));
-	}
+    constructor(articles: ReadonlyArray<ArticleVO>, count: number, split: number) {
+        const page = Math.ceil(count / split);
+        this.pages = new Array(page).fill(0).map((_, i) => i);
+        this.list = articles.map(article => new Article(article));
+    }
 }
--- src/app/services/articleListService.ts
+++ src/app/services/articleListService.ts
@@ -6,96 +6,97 @@ import Article from '../models/articleModel';
 import Service from '../libs/Service';
 
 export default class ArticleListService extends Service {
-  readonly request: ArticleRequest;
-  @observable private _query: string;
-  @observable private _articleList: ArticleList;
-  @observable private _selectedPage: number;
-  @observable private readonly limit: number;
+    readonly request: ArticleRequest;
+    @observable private _query: string;
+    @observable private _articleList: ArticleList;
+    @observable private _selectedPage: number;
+    @observable private readonly limit: number;
 
-  constructor(private readonly type: ArticleListServiceType) {
-    super();
-    this.request = ArticleRequest.instance;
-    switch (this.type) {
-      case ArticleListServiceType.Author:
-      case ArticleListServiceType.Like:
-        this.limit = 5;
-        break;
-      case ArticleListServiceType.Tag:
-      case ArticleListServiceType.FEED:
-        this.limit = 10;
-        break;
+    constructor(private readonly type: ArticleListServiceType) {
+        super();
+        this.request = ArticleRequest.instance;
+        switch (this.type) {
+            case ArticleListServiceType.Author:
+            case ArticleListServiceType.Like:
+                this.limit = 5;
+                break;
+            case ArticleListServiceType.Tag:
+            case ArticleListServiceType.FEED:
+                this.limit = 10;
+                break;
+        }
     }
-  }
-  async update() {
-    const {limit, offset} = this;
-    switch (this.type) {
-      case ArticleListServiceType.Author:
-        await this.loadInternal({author: this.query, limit, offset});
-        break;
-      case ArticleListServiceType.Tag:
-        await this.loadInternal({tag: this.query, limit, offset});
-        break;
-      case ArticleListServiceType.Like:
-        await this.loadInternal({favorited: this.query, limit, offset});
-        await this.loadInternal({favorited: this.query, limit, offset});
-        break;
-      case ArticleListServiceType.FEED:
-        await this.loadInternal({limit, offset});
-        break;
+
+    async update() {
+        const {limit, offset} = this;
+        switch (this.type) {
+            case ArticleListServiceType.Author:
+                await this.loadInternal({author: this.query, limit, offset});
+                break;
+            case ArticleListServiceType.Tag:
+                await this.loadInternal({tag: this.query, limit, offset});
+                break;
+            case ArticleListServiceType.Like:
+                await this.loadInternal({favorited: this.query, limit, offset});
+                await this.loadInternal({favorited: this.query, limit, offset});
+                break;
+            case ArticleListServiceType.FEED:
+                await this.loadInternal({limit, offset});
+                break;
+        }
     }
-  }
 
-  @computed get offset() {
-    if (this.selectedPage && !isNaN(this.selectedPage)) {
-      return this.selectedPage * this.limit;
-    } else {
-      return 0;
+    @computed get offset() {
+        if (this.selectedPage && !isNaN(this.selectedPage)) {
+            return this.selectedPage * this.limit;
+        } else {
+            return 0;
+        }
     }
-  }
 
-  @computed get list(): Array<Article> {
-    return this.articleList ? this.articleList.list : [];
-  }
+    @computed get list(): Array<Article> {
+        return this.articleList ? this.articleList.list : [];
+    }
 
-  @computed get pages(): ReadonlyArray<number> {
-    return this.articleList ? this.articleList.pages : [];
-  }
+    @computed get pages(): ReadonlyArray<number> {
+        return this.articleList ? this.articleList.pages : [];
+    }
 
-  @computed get selectedPage(): number {
-    return this._selectedPage || 0;
-  }
+    @computed get selectedPage(): number {
+        return this._selectedPage || 0;
+    }
 
-  @computed get query(): string {
-    return this._query;
-  }
+    @computed get query(): string {
+        return this._query;
+    }
 
-  set selectedPage(value: number) {
-    this._selectedPage = value;
-    this.update();
-  }
+    set selectedPage(value: number) {
+        this._selectedPage = value;
+        this.update();
+    }
 
-  set query(value: string) {
-    this._query = value;
-    this._selectedPage = 0;
-    this.update();
-  }
+    set query(value: string) {
+        this._query = value;
+        this._selectedPage = 0;
+        this.update();
+    }
 
-  @computed get articleList(): ArticleList {
-    return this._articleList;
-  }
+    @computed get articleList(): ArticleList {
+        return this._articleList;
+    }
 
-  set articleList(value: ArticleList) {
-    this._articleList = value;
-  }
+    set articleList(value: ArticleList) {
+        this._articleList = value;
+    }
 
-  private async loadInternal(param) {
-    const result = await this.request.loadArticlesExternal(param);
-    this.articleList = new ArticleList(result.articles, result.articlesCount);
-  }
+    private async loadInternal(param) {
+        const result = await this.request.loadArticlesExternal(param);
+        this.articleList = new ArticleList(result.articles, result.articlesCount, this.limit);
+    }
 
-  updateLike(i: number) {
-    const finish = raw => this.list[i] = new Article(raw);
-    const method = (this.list[i].isFavorite ? 'remove' : 'set') + 'Like';
-    this.request[method](this.list[i].id).then(finish);
-  }
+    updateLike(i: number) {
+        const finish = raw => this.list[i] = new Article(raw);
+        const method = (this.list[i].isFavorite ? 'remove' : 'set') + 'Like';
+        this.request[method](this.list[i].id).then(finish);
+    }
 }
