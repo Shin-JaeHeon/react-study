040c569ada96f50da897d6b9c42a8bf639d8fe9d
JaeHeonShin <sjh0624@midasit.com>
2019-11-25 15:39:40 +0900

    Refactoring

--- src/app/libs/VM.ts
+++ src/app/libs/VM.ts
@@ -2,21 +2,13 @@ import {computed, observable} from 'mobx';
 
 export default class VM<T> {
 
-    @observable private _serviceSeparator: number = 0;
+    @observable serviceSeparator: number = 0;
 
     constructor(private _service: T, private _service2: T = null) {
     }
 
-    get serviceSeparator(): number {
-        return this._serviceSeparator;
-    }
-
-    set serviceSeparator(value: number) {
-        this._serviceSeparator = value;
-    }
-
     @computed get service(): T {
-        return !this._serviceSeparator ? this._service : (this._service2 ? this._service2 : this._service);
+        return !this.serviceSeparator ? this._service : (this._service2 ? this._service2 : this._service);
     };
 
     setService(service: T, service2: T = null) {
