87bde5e9992a4e0282ba71b8f846dc8154c521fd
JaeHeonShin <sjh0624@midasit.com>
2019-11-25 12:17:14 +0900

    PopularTags Component로 분리

---
+++ src/app/gadgets/popularTags/popularTags.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {observer} from "mobx-react";
+import TagListItem from '../tagList/tagListItem';
+import * as style from './populartags.less';
+
+interface Props {
+    list: ReadonlyArray<string>;
+    handler: (tag: string) => () => void;
+}
+
+@observer
+export default class PopularTags extends React.Component<Props> {
+
+    render() {
+        const {handler, list} = this.props;
+        return (
+            <div className={style.tags}>
+                <h4>Popular Tags</h4>
+                {list.map((tag, i) => (
+                    <TagListItem tag={tag} key={i} onClick={handler(tag)}/>
+                ))}
+            </div>
+        );
+    }
+}
--- src/app/pages/main/Main.tsx
+++ src/app/pages/main/Main.tsx
@@ -5,52 +5,48 @@ import {observer} from "mobx-react";
 import FeedList from "../../gadgets/feedList/FeedList";
 import MainVM from "./mainVM";
 import TopUI from "../../gadgets/topUI/TopUI";
-import TagListItem from '../../gadgets/tagList/tagListItem';
+import PopularTags from '../../gadgets/popularTags/popularTags';
 
 @observer
 export default class Main extends React.Component {
-  readonly vm = MainVM.instance;
-  tagClicked = tag => () => this.vm.clickedTag = tag;
-  topPageClicked = page => () => {
-    if (page === 0) this.vm.clickedTag = '';
-    this.vm.topPage = page;
-  };
-  pageClicked = page => this.vm.page = page;
-  likeClicked = (i: number) => this.vm.updateLike(i);
+    readonly vm = MainVM.instance;
+    tagClicked = tag => () => this.vm.clickedTag = tag;
+    topPageClicked = page => () => {
+        if (page === 0) this.vm.clickedTag = '';
+        this.vm.topPage = page;
+    };
+    pageClicked = page => this.vm.page = page;
+    likeClicked = (i: number) => this.vm.updateLike(i);
 
-  componentDidMount() {
-    this.vm.init();
-  }
+    componentDidMount() {
+        this.vm.init();
+    }
 
-  render() {
-    const {topPage, articleList, popularTagList, pageList, topPageList} = this.vm;
-    return (
-      <main>
-        <div className={style.front}>
-          <div>
-            <h1>conduit</h1>
-            <div className={style.slogan}>A place to share your knowledge.</div>
-          </div>
-        </div>
-        <section>
-          <div className={style.feedContainer}>
-            <TopUI topPageList={topPageList}
-                   selectedPage={topPage}
-                   handler={this.topPageClicked}
-            />
-            <FeedList articleList={articleList} handler={this.likeClicked}/>
-          </div>
-          <div className={style.tags}>
-            <h4>Popular Tags</h4>
-            {popularTagList.map((tag, i) => (
-              <TagListItem tag={tag} key={i} onClick={this.tagClicked(tag)}/>
-            ))}
-          </div>
-        </section>
-        <footer>
-          <PageList pageList={pageList} handler={this.pageClicked}/>
-        </footer>
-      </main>
-    );
-  }
+    render() {
+        const {topPage, articleList, popularTagList, pageList, topPageList} = this.vm;
+        const {topPageClicked, likeClicked, tagClicked, pageClicked} = this;
+        return (
+            <main>
+                <div className={style.front}>
+                    <div>
+                        <h1>conduit</h1>
+                        <div className={style.slogan}>A place to share your knowledge.</div>
+                    </div>
+                </div>
+                <section>
+                    <div className={style.feedContainer}>
+                        <TopUI topPageList={topPageList}
+                               selectedPage={topPage}
+                               handler={topPageClicked}
+                        />
+                        <FeedList articleList={articleList} handler={likeClicked}/>
+                    </div>
+                    <PopularTags list={popularTagList} handler={tagClicked}/>
+                </section>
+                <footer>
+                    <PageList pageList={pageList} handler={pageClicked}/>
+                </footer>
+            </main>
+        );
+    }
 }
